73a620b40b0e863f188ce3f11bcba7f4cd8e1260
Manel Rodriguez <mrodriguez@creal.cat>
2010-11-04 16:30:57 +0100

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mccspain::Application.config.secret_token = '336acf16264af1bd53bc0a83609e19db0afd6f9100003b01c6ed9da5feb306e516b1013e9a102e1fae05789a9f8b2cb47460cdf714d5a49535f8f10d6d6b9076'
